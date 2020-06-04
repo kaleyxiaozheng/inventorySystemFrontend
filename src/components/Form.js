2beba5967193498aa6965c5689127737b915788b
kaleyxiaozheng <zheng.kaley.x@gmail.com>
2020-06-04 17:37:06 +1000

    frontend validation done

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,52 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
 export class LoginForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+  }
+
+  handleValidation() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Name
+    if (!fields["name"]) {
+      formIsValid = false;
+      errors["name"] = "User Name can not be empty";
+    }
+
+    if (!fields["password"]) {
+      formIsValid = false;
+      errors["password"] = "Password can not be empty";
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  contactSubmit(e) {
+    e.preventDefault();
+    if (this.handleValidation()) {
+      // alert("Form submitted");
+    } else {
+      // alert("Form has errors.")
+    }
+
+  }
+
+  handleChange(field, e) {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+  }
+
   render() {
     return (
       <div>
@@ -11,21 +57,26 @@ export class LoginForm extends Component {
           <input className={this.props.className} type={this.props.type} placeholder={this.props.text} />
         </form> */}
 
-        <form>
+        <form onSubmit= {this.contactSubmit.bind(this)}>
           <div className="lableForm">
             <label>User Name</label>
           </div>
           <div >
-            <input className="inputForm" type="text" placeholder="User name"></input>
+            <input className="inputForm" type="text" placeholder="User name" onChange={this.handleChange.bind(this, "name")} value={this.state.fields["name"]}></input>
+            <span className="error">{this.state.errors["name"]}</span>
           </div>
           <div className="lableForm">
-          <label>Password</label>
+            <label>Password</label>
           </div>
           <div ></div>
-          <input className="inputForm" type="text" placeholder="Password"></input>
+          <input className="inputForm" type="text" placeholder="Password" onChange={this.handleChange.bind(this, "password")} value={this.state.fields["password"]}></input>
+          <span className="error">{this.state.errors["password"]}</span>
           <div className="checkboxForm">
             <input className="checkboxCheck" type="checkbox"></input>
             <label className="rembMe" >Remember me</label>
+          </div>
+          <div className="submitLabel">
+            <input className="submitInput" type="submit"></input>
           </div>
         </form>
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../css/Login.css';
-import { LoginForm} from "./Form";
+import { LoginForm } from "./Form";
 import { LabelTitle } from "./Label";
 import { SubmitButtonRouter, LabelButtonRouter } from './Button';
 
@@ -16,39 +16,10 @@ export class Login extends Component {
                         <div className="adminTitle">
                             <h1><LabelTitle text="infoLabel" label="Sign in"></LabelTitle></h1>
                         </div>
-                        {/* <div className="infoLabel">
-                            <LabelTitle text="infoLabel" label="User Name"></LabelTitle>
-                        </div>
-                        <div className="formLabel">
-                            <Form type="text" text="User Name" value={this.state.username} ></Form>
-                        </div>
-                        <div className="errorLabel">
-                            {this.state.usernameError}
-                        </div>
-                        <div className="infoLabel">
-                            <LabelTitle text="infoLabel" label="Password"></LabelTitle>
-                        </div>
-                        <div className="formLabel">
-                            <Form type="password" text="Password" value={this.state.password} ></Form>
-                        </div>
-                        <div className="errorLabel">
-                            {this.state.passwordError}
-                        </div>
-                        <div className="checkboxArea">
-                            <div className="checkboxLabel">
-                                <Form type="checkbox"></Form>
-                            </div>
-                            <div className="checkboxTitle">
-                                <LabelTitle text="infoLabel" label="Remember me"></LabelTitle>
-                            </div>
-                        </div>
-                        <div className="buttonLabel">
-                            <SubmitButtonRouter text="submitButton" label="Submit" path="/Submit" ></SubmitButtonRouter>
-                            <Form className="submitLabel" type="submit" text="Submit"></Form>
-                        </div> */}
-
                         <LoginForm></LoginForm>
-
+                        <div>
+                            {/* <SubmitButtonRouter text="submitButton" label="Submit"></SubmitButtonRouter> */}
+                        </div>
                         <div className="forgetLabel">
                             <div className="left"></div>
                             <div className="right">
