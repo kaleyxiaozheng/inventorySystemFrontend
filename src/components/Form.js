6c0b40b3794299c69bbb42fea1b4a1fb7ed78a40
kaleyxiaozheng <zheng.kaley.x@gmail.com>
2020-06-04 13:10:19 +1000

    frontend validation v1.0

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../css/Form.css';  
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 export class Form extends Component {
   render() {
     return (
       <div className="inputForm">
         <form> 
-          <input type={this.props.type} placeholder={this.props.text} />
+          <input type={this.props.type} placeholder={this.props.text} onSubmit={this.props.handler} />
         </form>
       </div>
     );
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,56 @@ import { Form } from "./Form";
 import { LabelTitle } from "./Label";
 import { SubmitButtonRouter, LabelButtonRouter } from './Button';
 
+const initialState = {
+    username: "",
+    password: "",
+    usernameError: "",
+    passwordError: ""
+};
+
 export class Login extends Component {
+    
+    state = initialState
+
+    // handleChange = event => {
+    //     const isCheckbox = event.target.type === "checkbox";
+    //     this.setState({
+    //         [event.target.username]: isCheckbox
+    //             ? event.target.checked
+    //             : event.target.value
+    //     });
+    // };
+
+    // validate = () => {
+    //     let usernameError = "";
+    //     let passwordError = "";
+
+    //     if (isNaN(this.state.username)) {
+    //         usernameError = "User Name can not be empty";
+    //         passwordError = "Password can not be empty";
+    //     }
+
+    //     if (usernameError) {
+    //         this.setState({ usernameError });
+    //         return false;
+    //     }
+
+    //     if (passwordError) {
+    //         this.setState({ passwordError });
+    //         return false;
+    //     }
+    //     return true;
+    // };
+
+    // handleSubmit = event => {
+    //     event.preventDefault();
+    //     const isValid = this.validate();
+    //     if (!isValid) {
+    //         console.log(this.state);
+    //     }
+    //     console.log(this.state);
+    // };
+
     render() {
         return (
             <div className="bodyArea adminArea">
@@ -13,30 +62,37 @@ export class Login extends Component {
                     <div className="innerLeft"></div>
                     <div className="innerCentre">
                         <div className="adminTitle">
-                            <h1><LabelTitle label="Sign in"></LabelTitle></h1>
+                            <h1><LabelTitle text="infoLabel" label="Sign in"></LabelTitle></h1>
                         </div>
                         <div className="infoLabel">
-                            <LabelTitle label="User Name"></LabelTitle>
+                            <LabelTitle text="infoLabel" label="User Name"></LabelTitle>
                         </div>
                         <div className="formLabel">
-                            <Form type="text" text="User Name"></Form>
+                            <Form type="text" text="User Name" value={this.state.username} ></Form>
+                        </div>
+                        <div className="errorLabel">
+                            {this.state.usernameError}
                         </div>
                         <div className="infoLabel">
-                            <LabelTitle label="Password"></LabelTitle>
+                            <LabelTitle text="infoLabel" label="Password"></LabelTitle>
                         </div>
                         <div className="formLabel">
-                            <Form type="password" text="Password"></Form>
+                            <Form type="password" text="Password" value={this.state.password} ></Form>
+                        </div>
+                        <div className="errorLabel">
+                            {this.state.passwordError}
                         </div>
                         <div className="checkboxArea">
                             <div className="checkboxLabel">
                                 <Form type="checkbox"></Form>
                             </div>
                             <div className="checkboxTitle">
-                                <LabelTitle label="Remember me"></LabelTitle>
+                                <LabelTitle text="infoLabel" label="Remember me"></LabelTitle>
                             </div>
                         </div>
                         <div className="buttonLabel">
-                            <SubmitButtonRouter text="submitButton" label="Submit" path="/Submit"></SubmitButtonRouter>
+                            {/* <SubmitButtonRouter text="submitButton" label="Submit" path="/Submit" ></SubmitButtonRouter> */}
+                            <Form type="submit" text="Submit"></Form>
                         </div>
                         <div className="forgetLabel">
                             <div className="left"></div>
