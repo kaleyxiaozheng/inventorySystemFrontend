4debccea8a3fbc58565d8ec591e81fc22a5634d6
kaleyxiaozheng <zheng.kaley.x@gmail.com>
2020-06-09 14:59:23 +1000

    change client's logo

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import '../css/Body.css';
-import clientLogo from '../logo/ClientLogo.png';
+import clientLogo from '../logo/adbLogo.jpeg';
 
 export class Body extends Component {
     render() {
         return (
             <div className="bodyArea">
-                {/* <img className="clientLogo" src={clientLogo} /> */}
-                <h1>ADB</h1>
+                <img className="clientLogo" src={clientLogo} />
             </div>
         );
     }
