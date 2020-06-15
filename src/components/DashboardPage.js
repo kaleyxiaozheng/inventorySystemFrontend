3bc819a156fde439a275e20435c8353da723a77c
kaleyxiaozheng <zheng.kaley.x@gmail.com>
2020-06-15 22:38:20 +1000

    change dashbaord page display

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -2,6 +2,6 @@ import React from 'react';
 import '../css/DashboardPage.css';
 
 export const DashboardPage = () => (
-  <div className="bodyArea">aabb
+  <div className="bodyArea">dashboard page
   </div>
 )
